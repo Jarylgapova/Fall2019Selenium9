8ee66d4cf6e45d9db7fbf80862cfc55e2ae34588
Jarylgapova <Karima.jarylgapova@gmail.com>
2020-03-01 13:15:19 -0600

    First work 2

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -1,19 +1,37 @@
 package com.automation.tests.day2;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
 
-    public static void main(String[] args) {
-        // to start selenium scrip t we need:
-        // setup webdriver (browser driver) and create webdriver object
-
+    public static void main(String[] args) throws Exception {
+        //to start selenium script we need:
+        //setup webdriver (browser driver) and create webdriver object
         WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
+        //In selenium, everything starts from WebDriver interface'
+        //ChromeDriver extends RemoteWebDriver --> implements WebDriver
+        driver.get("http://google.com");//to open a website
+        Thread.sleep(3000);//for demo, wait 3 seconds
+        //method that return page title
+        //you can also see it as tab name, in the browser
+        String title = driver.getTitle();//returns <title>Some title</title> text
 
-        // in selenium, everything starts from WebDriver interface
 
-        driver.get("http://google.com"); // to open ghrom
+        String expectedTitle = "Google";
+        System.out.println("Title is..."+title);
+
+        if (expectedTitle.equals(title)){
+            System.out.println("Test passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+
+
+
+        driver.close();//to close browser
+        //browser cannot close itself
     }
 }
