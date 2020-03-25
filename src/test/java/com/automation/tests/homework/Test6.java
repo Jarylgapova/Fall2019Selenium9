c660aedaa0c36f111b49353e7d8d8930274e8682
Jarylgapova <Karima.jarylgapova@gmail.com>
2020-03-25 17:42:03 -0500

    day13

--- src/test/java/com/automation/tests/homework/Test6.java
+++ src/test/java/com/automation/tests/homework/Test6.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -81,67 +79,6 @@ public class Test6 {
         assertEquals(actual2,expected2);
     }
 
-    @Test
-    public void Test6() throws Exception {
-        //Test case #6
-        //Step 1. Go to "https://www.tempmailaddress.com/"
-        driver.get("https://www.tempmailaddress.com/");
-        driver.manage().window().maximize();
-        // Thread.sleep(3);
-        String email = driver.findElement(By.id("email")).getText();
-        //Step 2. Copy and save email as a string.
-        //Step 3. Then go to “https://practicecybertekschool.herokuapp.com”
-        driver.navigate().to("http://practice.cybertekschool.com/");
-        //Step 4. And click on “Sign Up For Mailing List".
-        driver.findElement(By.linkText("Sign Up For Mailing List")).click();
-        //Step 5. Enter any valid name.
-        driver.findElement(By.cssSelector("input[name='full_name']")).sendKeys("Maggie");
-        //Step 6. Enter email from the Step 2.
-        driver.findElement(By.cssSelector("input[name='email']")).sendKeys(email);
-        //Step 7. Click Sign Up
-        driver.findElement(By.cssSelector("button[type='submit']")).click();
-        //Step 8. Verify that following message is displayed:
-        String actual = driver.findElement(By.cssSelector("h3[class='subheader']")).getText();
-        String expected = "Thank you for signing up. Click the button below to return to the home page.";
-        Assert.assertEquals(actual, expected);
-        //“Thank you for signing up. Click the button below to
-        //return to the home page.”
-        //Step 9. Navigate back to the “https://
-        //www.tempmailaddress.com/”
-        //driver.navigate().to("https://www.tempmailaddress.com/");
-        driver.navigate().back();    //  ZDES' NADO TAK DELAT POTOMU WTO ESLI TY PROSTO NOVIGATE().TO" ...."
-        BrowserUtils.wait(1);// ON BUDET OTKRYVAT ETU STRANICU ZANOVO I TAM UJE DRUGOY EMAIL
-
-        driver.navigate().back();  //NAM NUJNO 3 RAZA BACK SDELAT CHTOBY DOYTI IMENNO DO TOY STRANICY CHTOB IMENNO TOT EMAIL KOTORIY TY ISPOLZOVALA
-        BrowserUtils.wait(1);
-
-        driver.navigate().back();
-        BrowserUtils.wait(1);
-
-        driver.navigate().refresh();
-        BrowserUtils.wait(2);
-
-        //Step 10. Verify that you’ve received an email from
-        //“do-not-reply@practice.cybertekschool.com”
-//        driver.findElement(By.cssSelector("a[class='navbar-brand']")).click();
-
-
-        Thread.sleep(5);
-        //Step 11. Click on that email to open it.
-        Actions actions = new Actions(driver);
-        WebElement elementLocator = driver.findElement(By.xpath("//table//tbody//tr[1]//td[1]"));
-        actions.doubleClick(elementLocator).perform();
-        //Step 12. Verify that email is from: “do-notreply@practice.cybertekschool.com”
-        String expectedEmail="do-not-reply@practice.cybertekschool.com";
-        String actualEmail = driver.findElement(By.id("odesilatel")).getText();
-
-        Assert.assertEquals(actualEmail, expectedEmail);
-
-        //Step 13. Verify that subject is: “Thanks for
-        String expected2 = "Thanks for subscribing to practice.cybertekschool.com!";
-        String actual2 = driver.findElement(By.id("predmet")).getText();
-        assertEquals(actual2, expected2);
-    }
 
 
 
--- src/test/java/com/automation/tests/homework4/DayTest.java
+++ src/test/java/com/automation/tests/homework4/DayTest.java
@@ -5,6 +5,7 @@ import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,6 +15,8 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class DayTest {
 
@@ -22,18 +25,34 @@ public class DayTest {
     @Test
     public void dayTest(){
         driver.get("http://samples.gwtproject.org/samples/Showcase/Showcase.html#!CwCheckBox");
+        int i = 0;
+        String day= "";
 
-        BrowserUtils.wait(2);
-        List<WebElement> allCheck  =driver.findElements(By.xpath("//input[@type='checkbox']"));
-        for (WebElement each : allCheck) {
-            if(!each.isSelected()){
-                each.click();
+        while (i < 3) {
+            BrowserUtils.wait(2);
+            // Randomly select a checkbox.
+            List<WebElement> options = driver.findElements(By.cssSelector(".gwt-CheckBox>label"));
+            Random random = new Random();
+
+            int index = random.nextInt(options.size());
+            if (options.get(index).isEnabled()) {
+                options.get(index).click();
+
+                day = options.get(index).getText();
+
+                if (day.equals("Friday")) {
+
+                    i++;
+                }
+                options.get(index).click();
             }
         }
+
     }
 
+
     @Test
-    public void currentDay(){
+    public void currentDay() {
         driver.get("http://practice.cybertekschool.com/dropdown");
 
         // Create object of SimpleDateFormat class and decide the format
@@ -46,21 +65,38 @@ public class DayTest {
         String date1= dateFormat.format(date);
 
         // Print the Date
-        String year = driver.findElement(By.xpath("//*[@id=\"year\"]/option[1]")).getText();
-        String month = driver.findElement(By.xpath("//*[@id=\"month\"]/option[3]")).getText();
-        String day = driver.findElement(By.xpath("//*[@id=\"day\"]/option[23]")).getText();
+        WebElement year = driver.findElement(By.id("year"));
+        WebElement month = driver.findElement(By.id("month"));
+        WebElement day= driver.findElement(By.id("day"));
 
-        String date2 = year+"/"+month+"/"+day;
+        Select y = new Select(year);
+        Select m = new Select(month);
+        Select d = new Select(day);
+
+        String year1 = y.getFirstSelectedOption().getText();
+        String month1 = m.getFirstSelectedOption().getText();
+        String day1 = d.getFirstSelectedOption().getText();
+        String date2 = year1+"/"+month1+"/"+day1;
 
         Assert.assertEquals(date2,date1, "TEST FAILED TY DURA ");
         System.out.println(date2);
         System.out.println(date1);
     }
 
+    @Test
+    public void test3(){
+        driver.get("http://practice.cybertekschool.com/dropdown");
+
+
+
+    }
 
     @BeforeMethod
     public void setup(){
         driver = DriverFactory.createADriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
 
     }
 
